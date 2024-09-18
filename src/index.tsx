a9f4c173b3770ec827e9bd6a36bd0f0d095a4888
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-09-18 11:12:55 +0700

    configure browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,35 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import Webpack from './routes/webpack';
 
 const domNode = document.getElementById('root') as HTMLElement;
 const root = createRoot(domNode);
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Webpack />,
+      },
+      {
+        path: 'parcel',
+        element: <div>Parcel</div>,
+      },
+      {
+        path: 'esbuild',
+        element: <div>ESBuild</div>,
+      },
+    ],
+  },
+]);
+
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
