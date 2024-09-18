03d27aea96d14397d5137661c392f051345bb40e
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-09-19 06:57:36 +0700

    add supported webpack plugin list

---
+++ src/utils/webpack-plugins.ts
@@ -0,0 +1,30 @@
+export const webpackPlugins = {
+  'html-webpack-plugin': {
+    importDeclaration:
+      "const HTMLWebpackPlugin = require('html-webpack-plugin')",
+    pluginEntry: 'new HTMLWebpackPlugin()',
+  },
+  'html-bundle-analyzer': {
+    importDeclaration:
+      "const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin",
+    pluginEntry: `new BundleAnalyzerPlugin()`,
+  },
+  'mini-css-extract-plugin': {
+    importDeclaration:
+      "const MiniCssExtractPlugin = require('mini-css-extract-plugin')",
+    pluginEntry: 'new MiniCssExtractPlugin()',
+  },
+  'clean-webpack-plugin': {
+    importDeclaration:
+      "const { CleanWebpackPlugin } = require('clean-webpack-plugin')",
+    pluginEntry: 'new CleanWebpackPlugin()',
+  },
+  'terser-webpack-plugin': {
+    importDeclaration: "const TerserPlugin = require('terser-webpack-plugin')",
+    pluginEntry: 'new TerserPlugin()',
+  },
+  'dotenv-webpack': {
+    importDeclaration: "const Dotenv = require('dotenv-webpack')",
+    pluginEntry: 'new Dotenv()',
+  },
+};
