e7636e714132b2c8e63c851708cf7812c7b59fcb
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-09-25 14:42:17 +0700

    create chevron icons from SVG

---
+++ src/components/ChevronDown.tsx
@@ -0,0 +1,19 @@
+export function ChevronDown() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15"
+      height="15"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M6 9l6 6 6-6"
+      ></path>
+    </svg>
+  );
+}
---
+++ src/components/ChevronUp.tsx
@@ -0,0 +1,19 @@
+export function ChevronUp() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15"
+      height="15"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M6 15l6-6 6 6"
+      ></path>
+    </svg>
+  );
+}
