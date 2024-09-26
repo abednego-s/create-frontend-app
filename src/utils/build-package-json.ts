259ff67a7fac5a131629c496a988d750dd10fcf9
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-09-26 11:10:30 +0700

    refactor package json builder

--- src/utils/build-package-json.ts
+++ src/utils/build-package-json.ts
@@ -1,6 +1,8 @@
 import type { Options } from '../types';
 
-const packageJson = {
+type PackageConfig = typeof initialConfig;
+
+const initialConfig = {
   name: 'empty-project',
   version: '1.0.0',
   description: '',
@@ -13,18 +15,10 @@ const packageJson = {
   devDependencies: {},
 };
 
-function webpack(plugins: Options['plugins']) {
-  let config = {
-    ...packageJson,
-    scripts: {
-      ...packageJson.scripts,
-      build: 'webpack --mode production',
-    },
-    devDependencies: {
-      ...packageJson.devDependencies,
-      webpack: '^5.94.0',
-      'webpack-cli': '^5.1.4',
-    },
+function webpack(this: PackageConfig, plugins: Options['plugins']) {
+  this.scripts = {
+    ...this.scripts,
+    build: 'webpack --mode production',
   };
 
   if (plugins) {
@@ -39,292 +33,231 @@ function webpack(plugins: Options['plugins']) {
       return prev;
     }, {});
 
-    config = {
-      ...config,
-      devDependencies: {
-        ...config.devDependencies,
-        ...webpackPlugins,
-      },
+    this.devDependencies = {
+      ...this.devDependencies,
+      webpack: '^5.94.0',
+      'webpack-cli': '^5.1.4',
+      ...webpackPlugins,
     };
   }
-
-  return config;
 }
 
-function react() {
-  const config = {
-    ...packageJson,
-    scripts: {
-      ...packageJson.scripts,
-      dev: 'webpack serve --mode development',
-    },
-    dependencies: {
-      ...packageJson.dependencies,
-      react: '^18.3.1',
-      'react-dom': '^18.3.1',
-    },
-    devDependencies: {
-      ...packageJson.devDependencies,
-      'webpack-dev-server': '^5.1.0',
-    },
+// eslint-disable-next-line no-unused-vars
+function react(this: PackageConfig) {
+  this.scripts = {
+    ...this.scripts,
+    dev: 'webpack serve --mode development',
   };
 
-  return config;
-}
-
-function svelte() {
-  const config = {
-    ...packageJson,
-    scripts: {
-      ...packageJson.scripts,
-      dev: 'webpack serve --mode development',
-    },
-    devDependencies: {
-      ...packageJson.devDependencies,
-      svelte: '^4.2.19',
-      'svelte-loader': '^3.2.3',
-      'svelte-preprocess': '^6.0.2',
-      'webpack-dev-server': '^5.1.0',
-    },
+  this.dependencies = {
+    ...this.dependencies,
+    react: '^18.3.1',
+    'react-dom': '^18.3.1',
   };
 
-  return config;
+  this.devDependencies = {
+    ...this.devDependencies,
+    'webpack-dev-server': '^5.1.0',
+  };
 }
 
-function vue() {
-  const config = {
-    ...packageJson,
-    scripts: {
-      ...packageJson.scripts,
-      dev: 'webpack serve --mode development',
-    },
-    dependencies: {
-      ...packageJson.dependencies,
-      vue: '^3.5.8',
-    },
-    devDependencies: {
-      ...packageJson.devDependencies,
-      'vue-loader': '^17.4.2',
-      'vue-template-compiler': '^2.7.16',
-      'webpack-dev-server': '^5.1.0',
-    },
+// eslint-disable-next-line no-unused-vars
+function svelte(this: PackageConfig) {
+  this.scripts = {
+    ...this.scripts,
+    dev: 'webpack serve --mode development',
   };
 
-  return config;
+  this.devDependencies = {
+    ...this.devDependencies,
+    svelte: '^4.2.19',
+    'svelte-loader': '^3.2.3',
+    'svelte-preprocess': '^6.0.2',
+    'webpack-dev-server': '^5.1.0',
+  };
 }
 
-function babel(lib: Options['lib']) {
-  const config = {
-    ...packageJson,
-    devDependencies: {
-      ...packageJson.devDependencies,
-      '@babel/core': '^7.25.2',
-      '@babel/preset-env': '^7.25.4',
-    },
+// eslint-disable-next-line no-unused-vars
+function vue(this: PackageConfig) {
+  this.scripts = {
+    ...this.scripts,
+    dev: 'webpack serve --mode development',
+  };
+
+  this.dependencies = {
+    ...this.dependencies,
+    vue: '^3.5.8',
+  };
+
+  this.devDependencies = {
+    ...this.devDependencies,
+    'vue-loader': '^17.4.2',
+    'vue-template-compiler': '^2.7.16',
+    'webpack-dev-server': '^5.1.0',
+  };
+}
+
+function babel(
+  this: PackageConfig,
+  lib: Options['lib'],
+  bundler: Options['bundler']
+) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    '@babel/core': '^7.25.2',
+    '@babel/preset-env': '^7.25.4',
   };
 
   if (lib === 'react') {
-    const config = {
-      ...packageJson,
-      devDependencies: {
-        ...packageJson.devDependencies,
-        'babel-loader': '^9.1.3',
-        '@babel/preset-react': '^7.24.7',
-      },
+    this.devDependencies = {
+      ...this.devDependencies,
+      '@babel/preset-react': '^7.24.7',
     };
-
-    return config;
   }
 
-  return config;
+  if (bundler === 'webpack') {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'babel-loader': '^9.1.3',
+    };
+  }
 }
 
-function typescript() {
-  const config = {
-    ...packageJson,
-    devDependencies: {
-      ...packageJson.devDependencies,
-      '@types/react': '^18.3.5',
-      '@types/react-dom': '^18.3.0',
-      typescript: '^5.6.2',
-      'ts-loader': '^9.5.1',
-    },
+// eslint-disable-next-line no-unused-vars
+function typescript(this: PackageConfig) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    '@types/react': '^18.3.5',
+    '@types/react-dom': '^18.3.0',
+    typescript: '^5.6.2',
+    'ts-loader': '^9.5.1',
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+function tailwind(this: PackageConfig) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    tailwindcss: 'latest',
+    autoprefixer: 'latest',
+    postcss: 'latest',
+    'postcss-loader': 'latest',
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+function materialUi(this: PackageConfig) {
+  this.dependencies = {
+    ...this.dependencies,
+    '@mui/material': 'latest',
+    '@emotion/react': 'latest',
+    '@emotion/styled': 'latest',
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+function cssStyleLoader(this: PackageConfig) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    'css-loader': '^7.1.2',
+    'style-loader': '^4.0.0',
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+function less(this: PackageConfig) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    'less-loader': 'latest',
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+function scss(this: PackageConfig) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    'sass-loader': 'latest',
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+function fileLoader(this: PackageConfig) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    'file-loader': '^6.2.0',
+  };
+}
+
+function jest(this: PackageConfig, transpiler: Options['transpiler']) {
+  this.scripts = {
+    ...this.scripts,
+    test: 'jest',
   };
 
-  return config;
-}
-
-function tailwind() {
-  const config = {
-    ...packageJson,
-    devDependencies: {
-      ...packageJson.devDependencies,
-      tailwindcss: 'latest',
-      autoprefixer: 'latest',
-      postcss: 'latest',
-      'postcss-loader': 'latest',
-    },
-  };
-
-  return config;
-}
-
-function materialUi() {
-  const config = {
-    ...packageJson,
-    dependencies: {
-      ...packageJson.dependencies,
-      '@mui/material': 'latest',
-      '@emotion/react': 'latest',
-      '@emotion/styled': 'latest',
-    },
-  };
-
-  return config;
-}
-
-function cssStyleLoader() {
-  const config = {
-    ...packageJson,
-    devDependencies: {
-      ...packageJson.devDependencies,
-      'css-loader': '^7.1.2',
-      'style-loader': '^4.0.0',
-    },
-  };
-
-  return config;
-}
-
-function less() {
-  const config = {
-    ...packageJson,
-    devDependencies: {
-      ...packageJson.devDependencies,
-      'less-loader': 'latest',
-    },
-  };
-
-  return config;
-}
-
-function scss() {
-  const config = {
-    ...packageJson,
-    devDependencies: {
-      ...packageJson.devDependencies,
-      'sass-loader': 'latest',
-    },
-  };
-
-  return config;
-}
-
-function fileLoader() {
-  const config = {
-    ...packageJson,
-    devDependencies: {
-      ...packageJson.devDependencies,
-      'file-loader': '^6.2.0',
-    },
-  };
-
-  return config;
-}
-
-function jest(transpiler: Options['transpiler']) {
-  let config = {
-    ...packageJson,
-    scripts: {
-      ...packageJson.scripts,
-      test: 'jest',
-    },
-    devDependencies: {
-      ...packageJson.devDependencies,
-      jest: 'latest',
-    },
+  this.devDependencies = {
+    ...this.devDependencies,
+    jest: 'latest',
   };
 
   if (transpiler?.includes('babel')) {
-    config = {
-      ...config,
-      devDependencies: {
-        ...config.devDependencies,
-        'babel-jest': 'latest',
-      },
+    this.devDependencies = {
+      ...this.devDependencies,
+      'babel-jest': 'latest',
     };
   }
 
   if (transpiler?.includes('ts')) {
-    config = {
-      ...config,
-      devDependencies: {
-        ...config.devDependencies,
-        'ts-jest': 'latest',
-        '@types/jest': 'latest',
-      },
+    this.devDependencies = {
+      ...this.devDependencies,
+      'ts-jest': 'latest',
+      '@types/jest': 'latest',
     };
   }
-
-  return config;
 }
 
-function vitest(lib: Options['lib']) {
-  let config = {
-    ...packageJson,
-    scripts: {
-      ...packageJson.scripts,
-      test: 'vitest',
-    },
-    devDependencies: {
-      ...packageJson.devDependencies,
-      vitest: 'latest',
-    },
+function vitest(this: PackageConfig, lib: Options['lib']) {
+  this.scripts = {
+    ...this.scripts,
+    test: 'vitest',
+  };
+
+  this.devDependencies = {
+    ...this.devDependencies,
+    vitest: 'latest',
   };
 
   if (lib === 'react') {
-    config = {
-      ...config,
-      devDependencies: {
-        ...config.devDependencies,
-        jsdom: 'latest',
-        '@vitejs/plugin-react': 'latest',
-      },
+    this.devDependencies = {
+      ...this.devDependencies,
+      jsdom: 'latest',
+      '@vitejs/plugin-react': 'latest',
     };
   }
-
-  return config;
 }
 
-function esLint(transpiler: Options['transpiler']) {
-  const config = {
-    ...packageJson,
-    scripts: {
-      ...packageJson.scripts,
-      lint: `eslint 'src/**/*.{${getExtensions(transpiler).join(',')}}'`,
-      'lint:fix': `eslint --fix 'src/**/*.{${getExtensions(transpiler).join(',')}}}'`,
-    },
-    devDependencies: {
-      ...packageJson.devDependencies,
-      eslint: 'latest',
-    },
+function esLint(this: PackageConfig, transpiler: Options['transpiler']) {
+  this.scripts = {
+    ...this.scripts,
+    lint: `eslint 'src/**/*.{${getExtensions(transpiler).join(',')}}'`,
+    'lint:fix': `eslint --fix 'src/**/*.{${getExtensions(transpiler).join(',')}}}'`,
   };
-  return config;
+
+  this.devDependencies = {
+    ...this.devDependencies,
+    eslint: 'latest',
+  };
 }
 
-function prettier(transpiler: Options['transpiler']) {
-  const config = {
-    ...packageJson,
-    scripts: {
-      ...packageJson.scripts,
-      format: `prettier --write 'src/**/*.{${getExtensions(transpiler).join(',')}}'`,
-    },
-    devDependencies: {
-      ...packageJson.devDependencies,
-      prettier: 'latest',
-    },
+function prettier(this: PackageConfig, transpiler: Options['transpiler']) {
+  this.scripts = {
+    ...this.scripts,
+    format: `prettier --write 'src/**/*.{${getExtensions(transpiler).join(',')}}'`,
   };
-  return config;
+
+  this.devDependencies = {
+    ...this.devDependencies,
+    prettier: 'latest',
+  };
 }
 
 function getExtensions(transpiler: Options['transpiler']) {
@@ -345,160 +278,105 @@ function getExtensions(transpiler: Options['transpiler']) {
 }
 
 export function buildPackageJson(options: Options) {
-  let config = {
-    ...packageJson,
+  const packageJson = {
+    ...initialConfig,
   };
 
   if (options.name) {
-    config = {
-      ...config,
-      name: options.name,
-    };
+    packageJson.name = options.name;
   }
 
   if (options.bundler === 'webpack') {
-    config = {
-      ...config,
-      ...webpack(options.plugins),
-    };
+    webpack.call(packageJson, options.plugins);
   }
 
   if (options.lib === 'react') {
-    config = {
-      ...config,
-      ...react(),
-    };
+    react.call(packageJson);
   }
 
   if (options.lib === 'svelte') {
-    config = {
-      ...config,
-      ...svelte(),
-    };
+    svelte.call(packageJson);
   }
 
   if (options.lib === 'vue') {
-    config = {
-      ...config,
-      ...vue(),
-    };
+    vue.call(packageJson);
   }
 
   if (options.transpiler?.includes('babel')) {
-    config = {
-      ...config,
-      ...babel(options.lib),
-    };
+    babel.call(packageJson, options.lib, options.bundler);
   }
 
   if (options.transpiler?.includes('ts')) {
-    config = {
-      ...config,
-      ...typescript(),
-    };
+    typescript.call(packageJson);
   }
 
   if (options.ui?.includes('tailwind')) {
-    config = {
-      ...config,
-      ...tailwind(),
-    };
+    tailwind.call(packageJson);
   }
 
   if (options.ui?.includes('material-ui')) {
-    config = {
-      ...config,
-      ...materialUi(),
-    };
+    materialUi.call(packageJson);
   }
 
   if (options.styling) {
-    config = {
-      ...config,
-      ...cssStyleLoader(),
-    };
+    cssStyleLoader.call(packageJson);
 
     if (options.styling.includes('less')) {
-      config = {
-        ...config,
-        ...less(),
-      };
+      less.call(packageJson);
     }
 
     if (options.styling.includes('scss')) {
-      config = {
-        ...config,
-        ...scss(),
-      };
+      scss.call(packageJson);
     }
   }
 
   if (options.image) {
-    config = {
-      ...config,
-      ...fileLoader(),
-    };
-
-    if (options.testing?.includes('jest')) {
-      config = {
-        ...config,
-        ...jest(options.transpiler),
-      };
-    }
-
-    if (options.testing?.includes('vitest')) {
-      config = {
-        ...config,
-        ...vitest(options.lib),
-      };
-    }
-
-    if (options.linting?.includes('eslint')) {
-      config = {
-        ...config,
-        ...esLint(options.transpiler),
-      };
-    }
-
-    if (options.linting?.includes('prettier')) {
-      config = {
-        ...config,
-        ...prettier(options.transpiler),
-      };
-    }
-
-    const dependencies = Object.keys(config.dependencies)
-      .sort()
-      .reduce((prev, current) => {
-        prev = {
-          ...prev,
-          [current]:
-            packageJson.dependencies[
-              current as keyof typeof packageJson.dependencies
-            ],
-        };
-        return prev;
-      }, {});
-
-    const devDependencies = Object.keys(config.devDependencies)
-      .sort()
-      .reduce((prev, current) => {
-        prev = {
-          ...prev,
-          [current]:
-            packageJson.devDependencies[
-              current as keyof typeof packageJson.devDependencies
-            ],
-        };
-        return prev;
-      }, {});
-
-    config = {
-      ...config,
-      dependencies,
-      devDependencies,
-    };
-
-    return JSON.stringify(config, null, 2);
+    fileLoader.call(packageJson);
   }
+
+  if (options.testing?.includes('jest')) {
+    jest.call(packageJson, options.transpiler);
+  }
+
+  if (options.testing?.includes('vitest')) {
+    vitest.call(packageJson, options.lib);
+  }
+
+  if (options.linting?.includes('eslint')) {
+    esLint.call(packageJson, options.transpiler);
+  }
+
+  if (options.linting?.includes('prettier')) {
+    prettier.call(packageJson, options.transpiler);
+  }
+
+  const dependencies = Object.keys(packageJson.dependencies)
+    .sort()
+    .reduce((prev, current) => {
+      prev = {
+        ...prev,
+        [current]:
+          packageJson.dependencies[
+            current as keyof PackageConfig['dependencies']
+          ],
+      };
+      return prev;
+    }, {});
+
+  const devDependencies = Object.keys(packageJson.devDependencies)
+    .sort()
+    .reduce((prev, current) => {
+      prev = {
+        ...prev,
+        [current]:
+          packageJson.devDependencies[
+            current as keyof PackageConfig['devDependencies']
+          ],
+      };
+      return prev;
+    }, {});
+
+  packageJson.devDependencies = devDependencies;
+  packageJson.dependencies = dependencies;
+  return JSON.stringify(packageJson, null, 2);
 }
