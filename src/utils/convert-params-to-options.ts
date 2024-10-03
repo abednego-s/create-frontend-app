ea9f83bd79520b030759353b20e8c5877cb8b52d
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-10-03 11:51:45 +0700

    convert url search params to options

---
+++ src/utils/convert-params-to-options.ts
@@ -0,0 +1,14 @@
+import { isMultiOptionParam } from './is-multi-option-param';
+import type { Options } from '../types';
+
+export function convertParamsToOptions(searchParams: URLSearchParams) {
+  return Array.from(searchParams).reduce((prev, current) => {
+    const [key, value] = current as [keyof Options, string];
+    prev = {
+      ...prev,
+      [key]: isMultiOptionParam(key) ? value.split(',') : value,
+    };
+
+    return prev;
+  }, {} as Options);
+}
