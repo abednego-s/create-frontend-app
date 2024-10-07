575fbe1aa783977cf161b03da8b954e49371c22d
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-10-07 11:45:07 +0700

    remove strategy pattern

--- src/utils/builders/build-package-json.ts
+++ src/utils/builders/build-package-json.ts
@@ -1,22 +1,397 @@
-import { BabelStrategy } from '../strategies/babel-strategy';
-import { CssModuleStrategy } from '../strategies/css-module-strategy';
-import { CssStrategy } from '../strategies/css-strategy';
-import { EsLintStrategy } from '../strategies/eslint-strategy';
-import { FileLoaderStrategy } from '../strategies/file-loader-strategy';
-import { JestStrategy } from '../strategies/jest-strategy';
-import { LessStrategy } from '../strategies/less-strategy';
-import { MaterialUiStrategy } from '../strategies/material-ui-strategy';
-import { ParcelStrategy } from '../strategies/parcel-strategy';
-import { PrettierStrategy } from '../strategies/prettier-strategy';
-import { ReactStrategy } from '../strategies/react-strategy';
-import { SassStrategy } from '../strategies/sass-strategy';
-import { SvelteStrategy } from '../strategies/svelte-strategy';
-import { TailwindStrategy } from '../strategies/tailwind-strategy';
-import { TypescriptStrategy } from '../strategies/typescript-strategy';
-import { VitestStrategy } from '../strategies/vitest-strategy';
-import { VueStrategy } from '../strategies/vue-strategy';
-import { WebpackStrategy } from '../strategies/webpack-strategy';
 import type { Options, PackageConfig } from '../../types';
+import { getExtensions } from '../get-extensions';
+
+function applyWebpack(this: PackageConfig, plugins: Options['plugins']) {
+  this.scripts = {
+    ...this.scripts,
+    build: 'webpack --mode production',
+  };
+
+  this.devDependencies = {
+    ...this.devDependencies,
+    webpack: '^5.94.0',
+    'webpack-cli': '^5.1.4',
+  };
+
+  if (plugins) {
+    const webpackPlugins = plugins.reduce((prev, current) => {
+      if (current !== 'HotModuleReplacementPlugin') {
+        prev = {
+          ...prev,
+          [current]: 'latest',
+        };
+      }
+
+      return prev;
+    }, {});
+
+    this.devDependencies = {
+      ...this.devDependencies,
+      ...webpackPlugins,
+    };
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+function applyParcel(this: PackageConfig) {
+  this.scripts = {
+    ...this.scripts,
+    start: 'parcel src/index.html',
+    build: 'parcel build src/index.html',
+  };
+  this.devDependencies = {
+    ...this.devDependencies,
+    parcel: 'latest',
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+function applyReact(this: PackageConfig) {
+  this.scripts = {
+    ...this.scripts,
+    dev: 'webpack serve --mode development',
+  };
+  this.dependencies = {
+    ...this.dependencies,
+    react: '^18.3.1',
+    'react-dom': '^18.3.1',
+  };
+  this.devDependencies = {
+    ...this.devDependencies,
+    'webpack-dev-server': '^5.1.0',
+  };
+}
+
+function applySvelte(
+  this: PackageConfig,
+  { isWebpack, isParcel }: { isWebpack: boolean; isParcel: boolean }
+) {
+  if (isWebpack) {
+    this.scripts = {
+      ...this.scripts,
+      dev: 'webpack serve --mode development',
+    };
+
+    this.devDependencies = {
+      ...this.devDependencies,
+      'svelte-loader': '^3.2.3',
+      'svelte-preprocess': '^6.0.2',
+      'webpack-dev-server': '^5.1.0',
+    };
+  }
+
+  if (isParcel) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      '@parcel/transformer-svelte': 'latest',
+    };
+  }
+
+  this.dependencies = {
+    ...this.dependencies,
+    svelte: '^4.2.19',
+  };
+}
+
+function applyVue(
+  this: PackageConfig,
+  {
+    isWebpack,
+    isParcel,
+    isCss,
+  }: { isWebpack: boolean; isParcel: boolean; isCss: boolean }
+) {
+  if (isWebpack) {
+    this.scripts = {
+      ...this.scripts,
+      dev: 'webpack serve --mode development',
+    };
+    this.devDependencies = {
+      ...this.devDependencies,
+      vue: '^3.3.4',
+      'vue-loader': '^17.2.4',
+      'vue-template-compiler': '^2.7.14',
+      'webpack-dev-server': '^5.1.0',
+    };
+
+    if (isCss) {
+      this.devDependencies = {
+        ...this.devDependencies,
+        'vue-style-loader': '^7.1.2',
+      };
+    }
+  }
+
+  if (isParcel) {
+    this.scripts = {
+      ...this.scripts,
+      '@parcel/transformer-vue': 'latest',
+    };
+  }
+}
+
+function applyBabel(
+  this: PackageConfig,
+  { isWebpack, isReact }: { isWebpack: boolean; isReact: boolean }
+) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    '@babel/core': '^7.25.2',
+    '@babel/preset-env': '^7.25.4',
+  };
+
+  if (isReact) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      '@babel/preset-react': '^7.24.7',
+    };
+  }
+
+  if (isWebpack) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'babel-loader': '^9.1.3',
+    };
+  }
+}
+
+function applyTypescript(
+  this: PackageConfig,
+  {
+    isWebpack,
+    isBabel,
+    isReact,
+  }: { isWebpack: boolean; isBabel: boolean; isReact: boolean }
+) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    typescript: '^5.6.2',
+  };
+
+  if (isReact) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      '@types/react': '^18.3.5',
+      '@types/react-dom': '^18.3.0',
+    };
+  }
+
+  if (isWebpack) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'ts-loader': '^9.5.1',
+    };
+  }
+
+  if (isBabel) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      '@babel/preset-typescript': 'latest',
+    };
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+function applyTailwind(this: PackageConfig) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    tailwindcss: 'latest',
+    autoprefixer: 'latest',
+    postcss: 'latest',
+    'postcss-loader': 'latest',
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+function applyMaterialUi(this: PackageConfig) {
+  this.dependencies = {
+    ...this.dependencies,
+    '@mui/material': 'latest',
+    '@emotion/react': 'latest',
+    '@emotion/styled': 'latest',
+  };
+}
+
+function applyCss(this: PackageConfig, { isWebpack }: { isWebpack: boolean }) {
+  if (isWebpack) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'css-loader': '^7.1.2',
+      'style-loader': '^4.0.0',
+    };
+  }
+}
+
+function applyCssModule(
+  this: PackageConfig,
+  { isWebpack }: { isWebpack: boolean }
+) {
+  if (isWebpack) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'css-loader': '^7.1.2',
+      'style-loader': '^4.0.0',
+    };
+  }
+}
+
+function applyLess(this: PackageConfig, { isWebpack }: { isWebpack: boolean }) {
+  if (isWebpack) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'css-loader': '^7.1.2',
+      'style-loader': '^4.0.0',
+    };
+  }
+  this.devDependencies = {
+    ...this.devDependencies,
+    less: 'latest',
+  };
+}
+
+function applySass(this: PackageConfig, { isWebpack }: { isWebpack: boolean }) {
+  if (isWebpack) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'css-loader': '^7.1.2',
+      'style-loader': '^4.0.0',
+      'sass-loader': 'latest',
+    };
+  } else {
+    this.devDependencies = {
+      ...this.devDependencies,
+      sass: 'latest',
+    };
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+function applyFileLoader(this: PackageConfig) {
+  this.devDependencies = {
+    ...this.devDependencies,
+    'file-loader': '^6.2.0',
+  };
+}
+
+function applyJest(
+  this: PackageConfig,
+  { isBabel, isTypescript }: { isBabel: boolean; isTypescript: boolean }
+) {
+  this.scripts = {
+    ...this.scripts,
+    test: 'jest',
+  };
+
+  if (isBabel) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'babel-jest': 'latest',
+    };
+  }
+
+  if (isTypescript) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'ts-jest': 'latest',
+      '@types/jest': 'latest',
+    };
+  }
+}
+
+function applyVitest(this: PackageConfig, { isReact }: { isReact: boolean }) {
+  this.scripts = {
+    ...this.scripts,
+    test: 'vitest',
+  };
+
+  this.devDependencies = {
+    ...this.devDependencies,
+    vitest: 'latest',
+  };
+
+  if (isReact) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      jsdom: 'latest',
+      '@vitejs/plugin-react': 'latest',
+    };
+  }
+}
+
+function applyEsLint(
+  this: PackageConfig,
+  {
+    isBabel,
+    isTypescript,
+    isReact,
+    isSvelte,
+  }: {
+    isBabel: boolean;
+    isTypescript: boolean;
+    isReact: boolean;
+    isSvelte: boolean;
+  }
+) {
+  const options = {
+    isBabel,
+    isTypescript,
+  };
+
+  this.scripts = {
+    ...this.scripts,
+    lint: `eslint 'src/**/*.{${getExtensions(options).join(',')}}'`,
+    'lint:fix': `eslint --fix 'src/**/*.{${getExtensions(options).join(',')}}}'`,
+  };
+
+  this.devDependencies = {
+    ...this.devDependencies,
+    eslint: 'latest',
+    'eslint-config-prettier': 'latest',
+  };
+
+  if (isReact) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'eslint-plugin-react': 'latest',
+      'eslint-plugin-react-hooks': 'latest',
+    };
+  }
+
+  if (isSvelte) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      'eslint-plugin-svelte3': 'latest',
+    };
+  }
+
+  if (isTypescript) {
+    this.devDependencies = {
+      ...this.devDependencies,
+      '@typescript-eslint/parser': 'latest',
+      '@typescript-eslint/eslint-plugin': 'latest',
+    };
+  }
+}
+
+function applyPrettier(
+  this: PackageConfig,
+  { isBabel, isTypescript }: { isBabel: boolean; isTypescript: boolean }
+) {
+  const options = {
+    isBabel,
+    isTypescript,
+  };
+
+  this.scripts = {
+    ...this.scripts,
+    format: `prettier --write 'src/**/*.{${getExtensions(options).join(',')}}'`,
+  };
+
+  this.devDependencies = {
+    ...this.devDependencies,
+    prettier: 'latest',
+  };
+}
 
 export function buildPackageJson(options: Options) {
   const packageJson: PackageConfig = {
@@ -45,8 +420,8 @@ export function buildPackageJson(options: Options) {
     ui,
   } = options;
 
-  const isBabel = transpiler?.includes('babel');
-  const isTypescript = transpiler?.includes('ts');
+  const isBabel = transpiler?.includes('babel') ?? false;
+  const isTypescript = transpiler?.includes('ts') ?? false;
 
   const isReact = lib === 'react';
   const isSvelte = lib === 'svelte';
@@ -55,114 +430,106 @@ export function buildPackageJson(options: Options) {
   const isParcel = bundler === 'parcel';
   const isWebpack = bundler === 'webpack';
 
-  const isTailwind = ui?.includes('tailwind');
-  const isMaterialUi = ui?.includes('material-ui');
+  const isTailwind = ui?.includes('tailwind') ?? false;
+  const isMaterialUi = ui?.includes('material-ui') ?? false;
 
-  const isCss = styling?.includes('css');
-  const isCssModule = styling?.includes('css-module');
-  const isLess = styling?.includes('less');
-  const isSass = styling?.includes('scss');
+  const isCss = styling?.includes('css') ?? false;
+  const isCssModule = styling?.includes('css-module') ?? false;
+  const isLess = styling?.includes('less') ?? false;
+  const isSass = styling?.includes('scss') ?? false;
 
-  const isJest = testing?.includes('jest');
-  const isVitest = testing?.includes('vitest');
+  const isJest = testing?.includes('jest') ?? false;
+  const isVitest = testing?.includes('vitest') ?? false;
 
-  const isEslint = linting?.includes('eslint');
-  const isPrettier = linting?.includes('prettier');
+  const isEslint = linting?.includes('eslint') ?? false;
+  const isPrettier = linting?.includes('prettier') ?? false;
 
   if (name) {
     packageJson.name = name;
   }
 
   if (isWebpack) {
-    new WebpackStrategy({
-      plugins,
-    }).applyPackageConfig(packageJson);
+    applyWebpack.call(packageJson, plugins);
   }
 
   if (isParcel) {
-    new ParcelStrategy().applyPackageConfig(packageJson);
+    applyParcel.call(packageJson);
   }
 
   if (isReact) {
-    new ReactStrategy().applyPackageConfig(packageJson);
+    applyReact.call(packageJson);
   }
 
   if (isSvelte) {
-    new SvelteStrategy({ isWebpack, isParcel }).applyPackageConfig(packageJson);
+    applySvelte.call(packageJson, { isWebpack, isParcel });
   }
 
   if (isVue) {
-    new VueStrategy({ isWebpack, isParcel }).applyPackageConfig(packageJson);
+    applyVue.call(packageJson, { isWebpack, isParcel, isCss });
   }
 
   if (isBabel) {
-    new BabelStrategy({
-      isReact,
-      isWebpack,
-    }).applyPackageConfig(packageJson);
+    applyBabel.call(packageJson, { isWebpack, isReact });
   }
 
   if (isTypescript) {
-    new TypescriptStrategy({
+    applyTypescript.call(packageJson, {
       isBabel,
       isReact,
       isWebpack,
-    }).applyPackageConfig(packageJson);
+    });
   }
 
   if (isTailwind) {
-    new TailwindStrategy().applyPackageConfig(packageJson);
+    applyTailwind.call(packageJson);
   }
 
   if (isMaterialUi) {
-    new MaterialUiStrategy().applyPackageConfig(packageJson);
+    applyMaterialUi.call(packageJson);
   }
 
   if (isCss) {
-    new CssStrategy({ isWebpack }).applyPackageConfig(packageJson);
+    applyCss.call(packageJson, { isWebpack });
   }
 
   if (isCssModule) {
-    new CssModuleStrategy({ isWebpack }).applyPackageConfig(packageJson);
+    applyCssModule.call(packageJson, { isWebpack });
   }
 
   if (isLess) {
-    new LessStrategy().applyPackageConfig(packageJson);
+    applyLess.call(packageJson, { isWebpack });
   }
 
   if (isSass) {
-    new SassStrategy({ isWebpack }).applyPackageConfig(packageJson);
+    applySass.call(packageJson, { isWebpack });
   }
 
   if (image) {
-    new FileLoaderStrategy({ assets: image }).applyPackageConfig(packageJson);
+    applyFileLoader.call(packageJson);
   }
 
   if (isJest) {
-    new JestStrategy({
-      isBabel,
-      isTypescript,
-    }).applyPackageConfig(packageJson);
+    applyJest.call(packageJson, { isBabel, isTypescript });
   }
 
   if (isVitest) {
-    new VitestStrategy({ isReact }).applyPackageConfig(packageJson);
+    applyVitest.call(packageJson, { isReact });
   }
 
   if (isEslint) {
-    new EsLintStrategy({
+    applyEsLint.call(packageJson, {
       isBabel,
       isReact,
       isSvelte,
       isTypescript,
-    }).applyPackageConfig(packageJson);
+    });
   }
 
   if (isPrettier) {
-    new PrettierStrategy({
+    applyPrettier.call(packageJson, {
       isBabel,
       isTypescript,
-    }).applyPackageConfig(packageJson);
+    });
   }
 
   const dependencies = Object.keys(packageJson.dependencies || {})
--- src/utils/builders/build-webpack-config.ts
+++ src/utils/builders/build-webpack-config.ts
@@ -1,18 +1,354 @@
 import { objectLiteralToString } from '../object-literals-to-string';
 import { webpackPlugins } from '../webpack-plugins';
-import { ReactStrategy } from '../strategies/react-strategy';
-import { SvelteStrategy } from '../strategies/svelte-strategy';
-import { VueStrategy } from '../strategies/vue-strategy';
-import { CssStrategy } from '../strategies/css-strategy';
-import { LessStrategy } from '../strategies/less-strategy';
-import { SassStrategy } from '../strategies/sass-strategy';
-import { FileLoaderStrategy } from '../strategies/file-loader-strategy';
-import { CssModuleStrategy } from '../strategies/css-module-strategy';
 import type {
   BuildConfig,
   WebpackConfig,
   WebpackBuildConfigOptions,
+  WebpackRuleSetRule,
 } from '../../types';
+
+function applyReact(
+  this: WebpackConfig,
+  { isBabel, isTypescript }: { isBabel: boolean; isTypescript: boolean }
+) {
+  this.module = {
+    ...this.module,
+  };
+
+  this.resolve = {
+    ...this.resolve,
+  };
+
+  if (isBabel && !isTypescript) {
+    const rule = {
+      test: /\.(js|jsx)$/,
+      use: {
+        loader: 'babel-loader',
+      },
+      exclude: /node_modules/,
+    };
+
+    this.module.rules = this.module.rules
+      ? [...this.module.rules, rule]
+      : [rule];
+
+    this.resolve.extensions = ['.js', '.jsx'];
+  }
+
+  if (isTypescript && !isBabel) {
+    const rule = {
+      test: /\.ts(x)?$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/,
+    };
+
+    this.module.rules = this.module.rules
+      ? [...this.module.rules, rule]
+      : [rule];
+
+    this.resolve.extensions = ['.ts', '.tsx', '.js'];
+  }
+
+  if (isBabel && isTypescript) {
+    const rule = {
+      test: /\.(js|ts)x?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
+    };
+
+    this.module.rules = this.module.rules
+      ? [...this.module.rules, rule]
+      : [rule];
+
+    this.resolve.extensions = ['.ts', '.tsx', '.js'];
+  }
+}
+
+function applySvelte(
+  this: WebpackConfig,
+  { isBabel, isTypescript }: { isBabel: boolean; isTypescript: boolean }
+) {
+  this.module = {
+    ...this.module,
+  };
+
+  if (!this.module.rules) {
+    this.module.rules = [];
+  }
+
+  this.resolve = {
+    ...this.resolve,
+  };
+
+  const extensions = ['.js', '.mjs', '.svelte'];
+
+  this.module.rules = [
+    ...this.module.rules,
+    {
+      test: /\.svelte$/,
+      use: {
+        loader: 'svelte-loader',
+        options: {
+          preprocess: isTypescript
+            ? '[code]sveltePreprocess({ typescript: true })[/code]'
+            : '[code]sveltePreprocess()[/code]',
+        },
+      },
+    },
+  ];
+
+  if (isBabel) {
+    this.module.rules = [
+      ...this.module.rules,
+      {
+        test: /\.(js|mjs)$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        exclude: /node_modules/,
+      },
+    ];
+  }
+
+  if (isTypescript) {
+    this.module.rules = [
+      ...this.module.rules,
+      {
+        test: /\.ts?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ];
+    extensions.unshift('.ts');
+  }
+
+  this.resolve = {
+    ...this.resolve,
+    alias: {
+      ...this.resolve.alias,
+      svelte: "[code]path.resolve('node_modules', 'svelte')[/code]",
+    },
+    extensions,
+    mainFields: ['svelte', 'browser', 'module', 'main'],
+  };
+}
+
+function applyVue(
+  this: WebpackConfig,
+  { isTypescript }: { isTypescript: boolean }
+) {
+  this.module = {
+    ...this.module,
+  };
+
+  if (!this.module.rules) {
+    this.module.rules = [];
+  }
+
+  this.resolve = {
+    ...this.resolve,
+  };
+
+  const extensions = ['.js', '.vue', '.json'];
+
+  this.module.rules?.push({
+    test: /\.vue$/,
+    loader: 'vue-loader',
+  });
+
+  if (!this.plugins) {
+    this.plugins = [];
+  }
+
+  this.plugins?.push('[code]new VueLoaderPlugin()[/code]' as '');
+
+  this.resolve.alias = {
+    vue$: 'vue/dist/vue.esm-bundler.js',
+  };
+
+  if (isTypescript) {
+    extensions.unshift('.ts');
+  }
+
+  this.resolve.extensions = extensions;
+}
+
+function applyCss(
+  this: WebpackConfig,
+  {
+    isVue,
+    useMiniCssExtractPlugin,
+  }: { isVue: boolean; useMiniCssExtractPlugin: boolean }
+) {
+  this.module = {
+    ...this.module,
+  };
+
+  if (!this.module.rules) {
+    this.module.rules = [];
+  }
+
+  let styleLoader = 'style-loader';
+
+  if (isVue) {
+    styleLoader = 'vue-style-loader';
+  }
+
+  let use = [styleLoader, 'css-loader'];
+
+  if (useMiniCssExtractPlugin) {
+    use = [
+      `[code]process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : '${styleLoader}'[/code]`,
+      'css-loader',
+    ];
+  }
+
+  this.module.rules.push({
+    test: /\.css$/,
+    use,
+  });
+}
+
+function applyCssModule(
+  this: WebpackConfig,
+  {
+    isVue,
+    useMiniCssExtractPlugin,
+  }: { isVue: boolean; useMiniCssExtractPlugin: boolean }
+) {
+  this.module = {
+    ...this.module,
+  };
+
+  if (!this.module.rules) {
+    this.module.rules = [];
+  }
+
+  let styleLoader = 'style-loader';
+
+  if (isVue) {
+    styleLoader = 'vue-style-loader';
+  }
+
+  if (useMiniCssExtractPlugin) {
+    styleLoader = `[code]process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : '${styleLoader}'[/code]`;
+  }
+
+  this.module.rules.push({
+    test: /\.module\.css$/,
+    use: [
+      styleLoader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+        },
+      },
+    ],
+  });
+
+  const updatedRules = this.module.rules?.map((moduleRule) => {
+    const rule = moduleRule as WebpackRuleSetRule;
+    if (rule?.test?.toString() === /\.css$/.toString()) {
+      return {
+        ...rule,
+        exclude: /\.module\.css$/,
+      };
+    }
+    return rule;
+  });
+
+  this.module.rules = updatedRules;
+}
+
+function applyLess(
+  this: WebpackConfig,
+  {
+    isVue,
+    useMiniCssExtractPlugin,
+  }: { isVue: boolean; useMiniCssExtractPlugin: boolean }
+) {
+  let styleLoader = 'style-loader';
+
+  if (isVue) {
+    styleLoader = 'vue-style-loader';
+  }
+
+  if (useMiniCssExtractPlugin) {
+    styleLoader = `[code]process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : '${styleLoader}'[/code]`;
+  }
+
+  this.module = {
+    ...this.module,
+  };
+
+  if (!this.module.rules) {
+    this.module.rules = [];
+  }
+
+  this.module.rules.push({
+    test: /\.less$/,
+    use: [styleLoader, 'css-loader', 'less-loader'],
+  });
+}
+
+function applySass(
+  this: WebpackConfig,
+  {
+    isVue,
+    useMiniCssExtractPlugin,
+  }: { isVue: boolean; useMiniCssExtractPlugin: boolean }
+) {
+  let styleLoader = 'style-loader';
+
+  if (isVue) {
+    styleLoader = 'vue-style-loader';
+  }
+
+  if (useMiniCssExtractPlugin) {
+    styleLoader = `[code]process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : '${styleLoader}'[/code]`;
+  }
+
+  this.module = {
+    ...this.module,
+  };
+
+  if (!this.module.rules) {
+    this.module.rules = [];
+  }
+
+  this.module?.rules?.push({
+    test: /\.scss$/,
+    use: [styleLoader, 'css-loader', 'sass-loader'],
+  });
+}
+
+function applyFileLoader(
+  this: WebpackConfig,
+  { assets }: { assets: string[] }
+): void {
+  this.module = {
+    ...this.module,
+  };
+
+  if (!this.module.rules) {
+    this.module.rules = [];
+  }
+
+  this.module.rules.push({
+    test: `[code]/\\.(${assets.join('|')})$/i[/code]`,
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    ],
+  });
+}
 
 function buildImports(options: WebpackBuildConfigOptions) {
   let template = '';
@@ -85,76 +421,61 @@ function buildConfig(options?: BuildConfig) {
     const isVue = lib === 'vue';
     const isSvelte = lib === 'svelte';
 
-    const isBabel = transpiler?.includes('babel');
-    const isTypescript = transpiler?.includes('ts');
+    const isBabel = transpiler?.includes('babel') ?? false;
+    const isTypescript = transpiler?.includes('ts') ?? false;
 
-    const isCss = styling?.includes('css');
-    const isCssModule = styling?.includes('css-module');
-    const isLess = styling?.includes('less');
-    const isSass = styling?.includes('scss');
+    const isCss = styling?.includes('css') ?? false;
+    const isCssModule = styling?.includes('css-module') ?? false;
+    const isLess = styling?.includes('less') ?? false;
+    const isSass = styling?.includes('scss') ?? false;
 
     const useMiniCssExtractPlugin =
       plugins?.includes('mini-css-extract-plugin') ?? false;
 
-    if (transpiler?.includes('ts')) {
+    if (isTypescript) {
       config.entry = './src/index.ts';
     }
 
     if (isReact) {
-      new ReactStrategy({
-        isBabel,
-        isTypescript,
-      }).applyWebpackConfig(config);
+      applyReact.call(config, { isBabel, isTypescript });
     }
 
     if (isSvelte) {
-      new SvelteStrategy({
-        isBabel,
-        isTypescript,
-      }).applyWebpackConfig(config);
+      applySvelte.call(config, { isBabel, isTypescript });
     }
 
     if (isVue) {
-      new VueStrategy({
-        isCss,
-        isTypescript,
-      }).applyWebpackConfig(config);
+      applyVue.call(config, { isCss, isTypescript });
     }
 
     if (isCss) {
-      new CssStrategy({
-        useMiniCssExtractPlugin,
+      applyCss.call(config, {
         isVue,
-      }).applyWebpackConfig(config);
+        useMiniCssExtractPlugin,
+      });
     }
 
     if (isCssModule) {
-      new CssModuleStrategy({
-        useMiniCssExtractPlugin,
-        isVue,
-      }).applyWebpackConfig(config);
+      applyCssModule.call(config, { isVue, useMiniCssExtractPlugin });
     }
 
     if (isLess) {
-      new LessStrategy({
-        useMiniCssExtractPlugin,
-        isVue,
-      }).applyWebpackConfig(config);
+      applyLess.call(config, { isVue, useMiniCssExtractPlugin });
     }
 
     if (isSass) {
-      new SassStrategy({
-        useMiniCssExtractPlugin,
+      applySass.call(config, {
         isVue,
-      }).applyWebpackConfig(config);
+        useMiniCssExtractPlugin,
+      });
     }
 
     if (image) {
-      new FileLoaderStrategy({ assets: image }).applyWebpackConfig(config);
+      applyFileLoader.call(config, { assets: image });
     }
 
     if (font) {
-      new FileLoaderStrategy({ assets: font }).applyWebpackConfig(config);
+      applyFileLoader.call(config, { assets: font });
     }
 
     if (optimization?.includes('split-vendors')) {
