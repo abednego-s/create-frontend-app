4c4b99a5c44d816e7fc1da75d685d9bec7cc802b
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-10-07 09:24:34 +0700

    refactor tailwind config builder

--- src/utils/builders/build-tailwind-config.ts
+++ src/utils/builders/build-tailwind-config.ts
@@ -2,14 +2,14 @@ import { objectLiteralToString } from '../object-literals-to-string';
 import type { Options } from '../../types';
 
 export function buildTailwindConfig(options: Options) {
-  let exts = ['js', 'jsx', 'html'].join(',');
+  let extensions = ['js', 'jsx', 'html'].join(',');
 
   if (options.transpiler?.includes('ts')) {
-    exts = ['ts', 'tsx', 'html'].join(',');
+    extensions = ['ts', 'tsx', 'html'].join(',');
   }
 
   const config = {
-    content: [`./src/**/*.{${exts}}`],
+    content: [`./src/**/*.{${extensions}}`],
     theme: {
       extend: {},
     },
